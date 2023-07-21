d39c82828fdb70b68fc9d57be676cee8a41c2b04
jasmpa <82006282+jasmpa-1777547@users.noreply.github.com>
2023-07-20 18:20:02 -0700

    Group & Individual Jest Test

--- my-app/src/App.test.js
+++ my-app/src/App.test.js
@@ -6,3 +6,36 @@ test('renders learn react link', () => {
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+
+// Example done in class
+describe('The App', () => {
+  test('renders correctly', () => {
+    render(<App />) // Render into testing DOM
+
+    screen.debug(); // Use for Debugging: Render DOM/HTML into console
+    
+    // Look into the DOM and expects to see the text "Hello World"
+    // in the DOM. If it finds it, it passes.
+    expect(screen.getByText("Hello World")).toBeInTheDocument();
+
+    // Assign a feature to the value 'button' and then to be
+    // called to click on it.
+    const button = screen.getByRole('button'); // query (access) DOM
+    userEvent.click(button); // send user events
+  })
+})
+
+// Individual example
+describe('The App', () => {
+  test('renders correctly', () => {
+    render(<App />) // Render into testing DOM
+
+    screen.debug(); // Use for Debugging: Render DOM/HTML into console
+
+    expect(screen.getAllByRole('checkbox').toBeChecked())
+
+    const checkbox = screen.getAllByRole('checkbox');
+    userEvent.click(checkbox);
+  })
+})
